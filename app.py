cc5f1cf9be4080590de179eaec9cd897f1f97dca
Nafiz Shahriar <106926528+ItsYourNafiz@users.noreply.github.com>
2024-12-11 02:11:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ import pandas as pd
 import numpy as np
 
 st.title(':herb: Plant Disease Detection')
+st.write("This is the App to detect Plant disease by given a example image:")
+
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
+
+if picture:
+    st.image(picture)
+
